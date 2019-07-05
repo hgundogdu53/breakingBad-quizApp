85eebb6e6ee8e905b03bfdf6b0b9c04e1a0234e2
hgundogdu53 <38477613+hgundogdu53@users.noreply.github.com>
2019-07-05 15:30:10 -0700

    Add files via upload

---
+++ datastore.js
@@ -0,0 +1,93 @@
+//BreakingBad question and answer data sore//
+const STORE = [
+{
+question: "In what New Mexico city does 'Breaking Bad' take place?",
+ans1: 'Santa Fe',
+ans2: 'Albuquerque',
+ans3: 'Las Cruces',
+ans4: 'Roswell',
+
+correctAnswer: 'Albuquerque',
+},
+{
+question: 'Walt is a teacher of what high school subject?',
+ans1: 'Chemistry',
+ans2: 'Biology',
+ans3: 'Physics',
+ans4: 'Geology',
+
+correctAnswer: 'Chemistry',
+},
+{
+question: "What's the slogan of Saul Goodman's law firm?",
+ans1: 'Turn Your Pain Into Rain.',
+ans2: 'One Call…That’s All!',
+ans3: 'Better Call Saul!',
+ans4: 'Get What’s Yours.',
+
+correctAnswer: 'Better Call Saul!',
+},
+{
+question: "What weapons do Tuco's cousins use?",
+ans1: 'Chainsaws',
+ans2: 'Machetes',
+ans3: 'Guns',
+ans4: 'Axes',
+
+correctAnswer: 'Axes',
+},
+{
+question: "What's the name of the fried chicken restaurant owned by Gus Fring?",
+ans1: 'Pollos Hermanos',
+ans2: 'Pollo Feliz',
+ans3: 'Delicioso Pollo',
+ans4: 'Amigos de Pollo',
+
+correctAnswer: 'Pollos Hermanos',
+},
+{
+question: 'Which character does not end up dying by the end of the series?',
+ans1: 'Uncle Jack',
+ans2: 'Skinny Pete',
+ans3: 'Walter White',
+ans4: 'Hank',
+
+correctAnswer: 'Skinny Pete',
+},
+{
+question: 'Jesse Pinkman refers to himself as this in the first season:',
+ans1: 'Master Chef',
+ans2: 'Chef-Boyy-R-Dee',
+ans3: 'Cap’n Cook',
+ans4: 'Da Swedish Chef',
+
+correctAnswer: 'Cap’n Cook',
+},
+{
+question: 'What did Mike Ehrmantraut do for a living before he started working for Gus Fring?',
+ans1: 'He was a lawyer',
+ans2: 'He was a cop',
+ans3: 'He was a prison guard',
+ans4: 'He was a doctor',
+
+correctAnswer: 'He was a cop',
+},
+{
+question: 'What item does Hank find that makes him realize Walt is Heisenberg?',
+ans1: 'His lab notes',
+ans2: 'His bag filled with cash',
+ans3: 'His blue meth',
+ans4: 'His copy of “Leaves of Grass”',
+
+correctAnswer: 'His copy of “Leaves of Grass”',
+},
+{
+question: 'What does Walt poison Brock with in the season 4 episode“End Times"',
+ans1: 'Ricin',
+ans2: 'Lily of the Valley',
+ans3: 'Nightshade',
+ans4: 'Arsenic',
+
+correctAnswer: 'Lily of the Valley',
+}
+];
---
+++ script.js
@@ -0,0 +1,93 @@
+let questionNumber = 0;
+let score = 0;
+
+function generateQuizApp () {
+  return `<form class="quiz-form">
+  <h2>${STORE[questionNumber].question}</h2>
+  <fieldset class= 'answersAndSubmit'>
+  <label class= 'dataStore ans1' >
+  <input type="radio" name="answer" value="${STORE[questionNumber].ans1}" required>
+  <span>${STORE[questionNumber].ans1}</span>
+  </label>
+  
+  <label class= 'dataStore ans2' >
+  <input type="radio" name="answer" value="${STORE[questionNumber].ans2}" required>
+  <span>${STORE[questionNumber].ans2}</span>
+  </label>
+
+  <label class='dataStore ans3' >
+  <input type="radio" name="answer" value="${STORE[questionNumber].ans3}" required>
+  <span>${STORE[questionNumber].ans3}</span>
+  </label>
+
+  <label class='dataStore ans4' >
+  <input type="radio" name="answer" value="${STORE[questionNumber].ans4}" required>
+  <span>${STORE[questionNumber].ans4}</span>
+  </label>
+
+  <button class="js-submit" id="submitQuestion">Submit</button>
+  </fieldset>
+  </form>`;
+}
+
+function startQuestionForm() {
+  $('.titleButton').on('click', '#startButton', event =>  {
+    $('.titleButton').hide();
+    $('.js-quiz').show();
+    $('#currentQuestion').text(1);
+  });
+}
+
+function renderQuizInHtml() {
+  $('.js-quiz').html(generateQuizApp());
+  $('#submitQuestion').on('click', checkAnswer);
+};
+
+function goToNextQuestion() {
+  if (questionNumber < STORE.length -1) {
+      questionNumber++;
+      $('#currentQuestion').text(questionNumber + 1);
+      renderQuizInHtml();
+  }
+  else {
+    showResults();
+    $('#restartButton').on('click',  event => {
+    location.reload();
+    });
+  }
+}
+
+function checkAnswer() {
+    let selectedAnswer = $("input[name='answer']:checked").val();
+    let correctAnswer = `${STORE[questionNumber].correctAnswer}`; 
+    if (selectedAnswer === correctAnswer) {
+      $('.js-quiz').html(`<div class = 'goodAnswer'><p>That's Right.</p>
+      <img src="../images/breaking-bad-gas-mask-aftersubmit.png" id='correct_Answer' alt= 'correctAnswerIcon'/>
+      <button id='nextQ'>Next</button></div>`);      
+      score++;
+      $('#currentScore').text(score);
+    } else {
+      $('.js-quiz').html(`<div class = 'badAnswer'><p>Nope! That's not Right. Correct answer is "${correctAnswer}."</p><img src= "../images/breaking-bad-wrong answer.jpg" id='wrong_Answer' alt= 'wrongAnswerIcon'/><button id='nextQ'>Next</button></div>`);
+    }
+  $('#nextQ').on('click', goToNextQuestion);
+}  
+
+function showResults() {
+  if (score >= 7){
+  $('.js-quiz').html(`<div class='resultPage'><h2>You finished the QUIZ and That's very good</h2><img src='images/finishing test.jpg' alt= 'very-good' id = 'lastImage'/><button type = 'restart' id = 'restartButton'>Restart</button></div>`);
+  }  else if (score >= 4 && score <7){
+      $('.js-quiz').html(`<div class='resultPage'><h2>You finished the QUIZ and That's not BAD.</h2>
+      <img src='images/finishing test.jpg' alt= 'not-bad' id = 'lastImage'/>
+      <button type = 'restart' id = 'restartButton'>Restart</button></div>`);
+  } else {
+      $('.js-quiz').html(`<div class='resultPage'><h2>You finished the QUIZ and That's really BAD.</h2>
+      <img src='images/finishing test.jpg' alt= 'really-bad' id = 'lastImage'/>
+      <button type = 'restart' id = 'restartButton'>Restart</button></div>`);
+  }
+} 
+
+function letsGoToTheQuiz () {
+  startQuestionForm();
+  renderQuizInHtml();
+}
+$("#startButton").on('click', letsGoToTheQuiz);
