57eed8538e534ad9a6dead14b47522306d7acfc6
hgundogdu53 <38477613+hgundogdu53@users.noreply.github.com>
2019-07-05 18:46:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -62,26 +62,26 @@ function checkAnswer() {
     let correctAnswer = `${STORE[questionNumber].correctAnswer}`; 
     if (selectedAnswer === correctAnswer) {
       $('.js-quiz').html(`<div class = 'goodAnswer'><p>That's Right.</p>
-      <img src="../images/breaking-bad-gas-mask-aftersubmit.png" id='correct_Answer' alt= 'correctAnswerIcon'/>
+      <img src="breaking-bad-gas-mask-aftersubmit.png" id='correct_Answer' alt= 'correctAnswerIcon'/>
       <button id='nextQ'>Next</button></div>`);      
       score++;
       $('#currentScore').text(score);
     } else {
-      $('.js-quiz').html(`<div class = 'badAnswer'><p>Nope! That's not Right. Correct answer is "${correctAnswer}."</p><img src= "../images/breaking-bad-wrong answer.jpg" id='wrong_Answer' alt= 'wrongAnswerIcon'/><button id='nextQ'>Next</button></div>`);
+      $('.js-quiz').html(`<div class = 'badAnswer'><p>Nope! That's not Right. Correct answer is "${correctAnswer}."</p><img src= "breaking-bad-wrong answer.jpg" id='wrong_Answer' alt= 'wrongAnswerIcon'/><button id='nextQ'>Next</button></div>`);
     }
   $('#nextQ').on('click', goToNextQuestion);
 }  
 
 function showResults() {
   if (score >= 7){
-  $('.js-quiz').html(`<div class='resultPage'><h2>You finished the QUIZ and That's very good</h2><img src='images/finishing test.jpg' alt= 'very-good' id = 'lastImage'/><button type = 'restart' id = 'restartButton'>Restart</button></div>`);
+  $('.js-quiz').html(`<div class='resultPage'><h2>You finished the QUIZ and That's very good</h2><img src="finishing test.jpg" alt= 'very-good' id = 'lastImage'/><button type = 'restart' id = 'restartButton'>Restart</button></div>`);
   }  else if (score >= 4 && score <7){
       $('.js-quiz').html(`<div class='resultPage'><h2>You finished the QUIZ and That's not BAD.</h2>
-      <img src='images/finishing test.jpg' alt= 'not-bad' id = 'lastImage'/>
+      <img src="finishing test.jpg" alt= 'not-bad' id = 'lastImage'/>
       <button type = 'restart' id = 'restartButton'>Restart</button></div>`);
   } else {
       $('.js-quiz').html(`<div class='resultPage'><h2>You finished the QUIZ and That's really BAD.</h2>
-      <img src='images/finishing test.jpg' alt= 'really-bad' id = 'lastImage'/>
+      <img src="finishing test.jpg" alt= 'really-bad' id = 'lastImage'/>
       <button type = 'restart' id = 'restartButton'>Restart</button></div>`);
   }
 } 
